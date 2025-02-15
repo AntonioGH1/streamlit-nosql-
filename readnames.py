dbad1f42c256c83af623f21be8d8d5aa781a6e7f
AntonioGH1 <antonioai-3000@hotmail.com>
2025-02-15 14:03:52 -0600

    Commit 3.2.1 - Button and Filter

--- readnames.py
+++ readnames.py
@@ -11,10 +11,13 @@ st.title("Buscador de Nombres")
 
 # Cargar datos
 names_data = load_data()
+# Asegurarse de que 'index' sea un valor numérico
+names_data['index'] = pd.to_numeric(names_data['index'], errors='coerce')
 
 # Inputs para definir el rango de búsqueda
 start_name = st.text_input("Nombre inicial:")
 end_name = st.text_input("Nombre final:")
+index_name = st.text_input("Numero a buscar: ")
 
 # Botón para ejecutar la búsqueda
 if st.button("Buscar"):
@@ -26,5 +29,20 @@ if st.button("Buscar"):
         st.dataframe(filtered_data)
     else:
         st.warning("Por favor, ingresa ambos nombres para definir el rango.")
+
+# Botón para ejecutar la búsqueda por número
+if st.button("Numero"):
+    if index_name:
+        try:
+            index_name = int(index_name)  # Convertir el input a número
+            filtered_data = names_data[
+                names_data['index'] >= index_name
+            ]
+            st.write(f"Mostrando nombres con número mayor o igual a **{index_name}**")
+            st.dataframe(filtered_data)
+        except ValueError:
+            st.warning("Por favor, ingresa un número válido.")
+    else:
+        st.warning("Por favor, ingresa un número para buscar.")
 
 st.dataframe(names_data)
