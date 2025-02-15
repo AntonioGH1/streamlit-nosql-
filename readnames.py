e287d2a8d124393832933897953822d708d2effa
AntonioGH1 <antonioai-3000@hotmail.com>
2025-02-15 13:09:34 -0600

    Commit 2 - Cache

--- readnames.py
+++ readnames.py
@@ -3,8 +3,19 @@ import pandas as pd
 import streamlit as st  
 
 names_link = 'dataset.csv'  
-
 names_data = pd.read_csv(names_link)
-st.title("Streamlit and pandas")  
+
+###
+
+@st.cache_data
+def load_data():
+    data = names_data
+    return data
+    #names_link = 'dataset.csv'
+    #return pd.read_csv(names_link)
+
+st.title("Streamlit and pandas")
+# Cargar datos con caché
+names_data = load_data()
 
 st.dataframe(names_data)
